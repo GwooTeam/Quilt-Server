755d1b7c4adeda8c0d74374305b3387a130db805
2youngjun <86882798+2youngjun@users.noreply.github.com>
2023-11-11 16:29:04 +0900

    Feat[#7] MAC, Sign 검증 메서드 및 Certificate 생성 메서드 모듈화

--- server/src/main/java/com/gwooteam/server/api/NodeApiController.java
+++ server/src/main/java/com/gwooteam/server/api/NodeApiController.java
@@ -3,7 +3,9 @@ package com.gwooteam.server.api;
 import com.gwooteam.server.domain.Node;
 import com.gwooteam.server.service.NodeService;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +28,7 @@ public class NodeApiController {
         return ResponseEntity.ok(svrPubK);
     }
 
+    // 임시 Server PubK, PriK 생성 로직
     @Data
     static class FetchSvrPubK {
         private String svrPubK;
@@ -36,7 +39,7 @@ public class NodeApiController {
     // Nonce
     @PostMapping("/node/{id}/generateNonce")
     public ResponseEntity<SaveNonce> enrollNonce (@PathVariable("id") Long id) {
-        String nonce = generateNonce();
+        String nonce = generateRandom(10);
         nodeService.saveNonce(id, nonce);
 
         SaveNonce saveNonce = new SaveNonce();
@@ -50,12 +53,12 @@ public class NodeApiController {
         private String nonce;
     }
 
-    private String generateNonce() {
+    private String generateRandom(Integer n) {
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder nonce = new StringBuilder();
         Random random = new Random();
-        int length = 10;
-        for (int i = 0; i < length; i++) {
+//        int length = 10;
+        for (int i = 0; i < n; i++) {
             int index = random.nextInt(characters.length());
             nonce.append(characters.charAt(index));
         }
@@ -68,31 +71,89 @@ public class NodeApiController {
 
         VerifyForm verifyForm = new VerifyForm();
 
-        // Save Node PubK(s), PubK(e)
+        // Save Node PubK_sign, PubK_encrypt
         Node node = nodeService.findOne(id);
         nodeService.savePubKeys(id, request.getNodeEncryptPubK(), request.getNodeSignPubK());
 
-        // 모듈화 예정
-        // Verify MAC
+        // request에서 추출
         String fetchNonce = nodeService.findOne(id).getNonce();
         String nodeSign = request.getNodeSign();
         String nodeMac = request.getNodeMac();
 
-        // Verify Sign
+        // 검증 결과 저장
+        Boolean resultMacVerification = verifyMac(fetchNonce, nodeMac);
+        Boolean resultSignVerification = verifySign(fetchNonce, nodeSign);
+
+        // 검증 결과에 따른 Certificate 생성
+        if (resultSignVerification == Boolean.TRUE && resultMacVerification == Boolean.TRUE) {
+            // Generate Certificates
+            Certificates certificates = generateCertificates(request.nodeEncryptPubK, request.nodeSignPubK, request.nodePublicIP);
+
+            // Save Certificates
 
 
-        // Generate Certificates(어떤 로직을 거쳐)
-        String encryptCertificate = "";
-        String signCertificate= "";
-
-        // Save Certificates
-        Certificates certificates = new Certificates();
-        certificates.encryptCertificate = encryptCertificate;
-        certificates.signCertificate = signCertificate;
-
-        // Send Certificates
-        return ResponseEntity.ok(certificates);
+            // Send Certificates
+            return ResponseEntity.ok(certificates);
+        } else {
+            // BadRequest 전달
+            return ResponseEntity.badRequest().build();
+        }
     }
+
+    // MAC 인증 로직
+    private Boolean verifyMac(String fetchNonce, String nodeMac) {
+
+        // Server 측 MAC 계산
+        String serverMac = "";
+
+        if (nodeMac ==  serverMac) {
+            return Boolean.TRUE;
+        } else {
+            return Boolean.FALSE;
+        }
+    }
+
+    // Sign 인증 로직
+    private Boolean verifySign(String fetchNonce, String nodeSign) {
+
+        // Server 측 Sign 계산
+        String serverSign = "";
+
+        if (nodeSign ==  serverSign) {
+            return Boolean.TRUE;
+        } else {
+            return Boolean.FALSE;
+        }
+    }
+
+    private Certificates generateCertificates(String pubK_encrypt, String pubK_sign, String publicIP) {
+        Certificates certificates = new Certificates();
+
+        CertificateEncrypt certificateEncrypt = new CertificateEncrypt();
+        certificateEncrypt.serialNumber = generateRandom(10);
+        certificateEncrypt.pubK_encrypt = pubK_encrypt;
+        certificateEncrypt.publicIP = publicIP;
+
+        CertificateSign certificateSign = new CertificateSign();
+        certificateSign.serialNumber = generateRandom(10);
+        certificateSign.pubK_sign = pubK_sign;
+        certificateSign.publicIP = publicIP;
+
+        // 각 Certificate를 하나의 스트링으로 변환
+
+
+        // Server Private Key로 서명
+
+
+        // Init Certificates
+        certificates.certificateEncrypt = certificateEncrypt;
+        certificates.certificateSign = certificateSign;
+
+        return certificates;
+    }
+
+    // 필요 시 Binary -> String 변환 함수
+
 
     @Data
     static class Verify {
@@ -100,16 +161,29 @@ public class NodeApiController {
         private String nodeSign;
         private String nodeEncryptPubK;
         private String nodeSignPubK;
+        // public IP
+        private String nodePublicIP;
+
+    }
+
+    @Data
+    static class CertificateSign {
+        private String serialNumber;
+        private String pubK_sign;
+        private String publicIP;
+    }
+
+    @Data
+    static class CertificateEncrypt {
+        private String serialNumber;
+        private String pubK_encrypt;
+        private String publicIP;
     }
 
     @Data
     static class Certificates {
-        // Certificate Class 정의 필요
-        private String signCertificate;
-        private String encryptCertificate;
+        // String으로 자료형 전환 예정
+        private CertificateEncrypt certificateEncrypt;
+        private CertificateSign certificateSign;
     }
-
-//    private Certificates generateCertificates() {
-//
-//    }
 }
