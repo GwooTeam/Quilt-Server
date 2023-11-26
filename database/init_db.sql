e66e4e4e7a1931d251c98bc970fa5064cf2f6b8b
2youngjun <86882798+2youngjun@users.noreply.github.com>
2023-11-26 23:52:56 +0900

    Feat[#7] Node 도메인 수정

--- database/init_db.sql
+++ database/init_db.sql
@@ -8,15 +8,15 @@ CREATE TABLE node_info (
     nodeID VARCHAR(10) NOT NULL,
     nodePW VARCHAR(10) NOT NULL,
     hostname VARCHAR(10) NOT NULL,
-    publicip VARCHAR(15) NOT NULL,
-    nonce VARCHAR(20),
-    mk VARCHAR(20) NOT NULL,
+    publicIP VARCHAR(15) NOT NULL,
+    nonce BINARY(16),
+    mk BINARY(16),
     serial_number VARCHAR(10),
     encrypt_pubK VARCHAR(20),
     sign_pubK VARCHAR(20),
     PRIMARY KEY (id)
 ) ENGINE=InnODB DEFAULT CHARSET=utf8;
 
-INSERT INTO node_info (nodeID, nodePW, hostname, publicIP, nonce, mk, serial_number, encrypt_pubK, sign_pubK) VALUES ('james010', '1111', 'lee', '111.111.111.111', '', 'firstMK', '', '', '');
-INSERT INTO node_info (nodeID, nodePW, hostname, publicIP, nonce, mk, serial_number, encrypt_pubK, sign_pubK) VALUES ('james8238', '2222', 'young', '222.222.222.222', '', 'secondMK', '', '', '');
-INSERT INTO node_info (nodeID, nodePW, hostname, publicIP, nonce, mk, serial_number, encrypt_pubK, sign_pubK) VALUES ('james4239', '3333', 'jun', '333.333.333.333', '', 'thirdMK', '', '', '');
+INSERT INTO node_info (nodeID, nodePW, hostname, publicIP, nonce, mk, serial_number, encrypt_pubK, sign_pubK) VALUES ('hwang98', 'pbd25q5', 'hwang', '64.19.87.11', '36ced25aeaee6bd6', '2a0b26c2c11796a1', '1', 'qwerqwerqwerqwer', 'qwerqwerqwerqwer');
+INSERT INTO node_info (nodeID, nodePW, hostname, publicIP, nonce, mk, serial_number, encrypt_pubK, sign_pubK) VALUES ('jaeseok00', 'a1mxafv', 'lee', '32.56.77.22', '470ce8524e32bd63', 'c4106887b1a4b71e', '2', 'qwerqwerqwerqwer', 'qwerqwerqwerqwer');
+INSERT INTO node_info (nodeID, nodePW, hostname, publicIP, nonce, mk, serial_number, encrypt_pubK, sign_pubK) VALUES ('han7890', 'p1o4qlyo', 'han', '84.35.12.55',  'd8afc2aa8ec76f1a', '0aa7a73c0e5b9110', '3', 'qwerqwerqwerqwer', 'qwerqwerqwerqwer');
