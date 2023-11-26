6daf58ff233fdfa806211514ac7b81f464730c79
2youngjun <86882798+2youngjun@users.noreply.github.com>
2023-11-26 23:52:13 +0900

    Fix[#7] 디버깅 코드 삭제

--- server/src/main/java/com/gwooteam/server/login/LoginController.java
+++ server/src/main/java/com/gwooteam/server/login/LoginController.java
@@ -31,7 +31,6 @@ public class LoginController {
         if (node == null) {
             return "/nodes/loginNodeErrorForm";
         }
-        logger.debug("🩵" + form.getNodeID());
 
         if (node.getNodePW().equals(form.getNodePW())) {
             return "redirect:/nodes";
